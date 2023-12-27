527726263904d40b9c49e0f1b1888726e54100a3
Ayomide Oluniyi <ayorg360@gmail.com>
2023-12-27 23:24:59 +0100

    chore: add vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import { createMetaManager } from 'vue-meta'
+import { inject } from '@vercel/analytics';
 
 import '@/assets/styles/main.scss'
 
 createApp(App)
   .use(store).use(router).use(createMetaManager())
   .mount('#app')
+
+inject();
